d51d55a9581f1e8016ebdecac992ec4195d53710
Thasnim Ahmed <147643930+thasnimahmed1@users.noreply.github.com>
2025-02-09 16:46:20 -0500

    Second commit

--- README.md
+++ README.md
@@ -1,2 +1,10 @@
 # Group8WSU
 Application repository for group 8 CSC 4110
+
+Repository Clone 1 
+Date: 02/09/2025
+
+Webpages page:
+-Authentication: 
+-Homepage: http://localhost:5173/
+-CoursePage: 
--- group-8-app/src/main.jsx
+++ group-8-app/src/main.jsx
@@ -1,10 +1,19 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import './index.css'
-import App from './App.jsx'
+import React from "react";
+import ReactDOM from "react-dom/client";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import Homepage from "./HomePage";
+import Authentication from "./Authentication";
+import CoursePage from "./CoursePage";
+import "./index.css";
 
-createRoot(document.getElementById('root')).render(
-  <StrictMode>
-    <App />
-  </StrictMode>,
-)
+ReactDOM.createRoot(document.getElementById("root")).render(
+  <React.StrictMode>
+    <Router>
+      <Routes>
+        <Route path="/" element={<Homepage />} />
+        <Route path="/auth" element={<Authentication />} />
+        <Route path="/courses" element={<CoursePage />} />
+      </Routes>
+    </Router>
+  </React.StrictMode>
+);
